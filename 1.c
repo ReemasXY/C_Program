df83a2c0cc18e218e9bf7aac4db793912e3a7861
ReemasXY <sameermhr345@gmail.com>
2023-12-20 17:01:52 +0545

    deleted

--- 1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <conio.h>
-int main(){
-     
-     int a;
-     printf("enter a number");
-     scanf("%d",&a);
-     return 0;
-}
--- 2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-
-	
-	int a ; 
-	scanf("%d",&a);
-	printf("autocomplete is very helpful in this laptop %d",a);
-	
-}
