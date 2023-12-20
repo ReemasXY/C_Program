35544f7a3533d2e8f943b4bee5919af4a476147a
ReemasXY <sameermhr345@gmail.com>
2023-12-20 17:00:01 +0545

    Initial commit

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+     
+     int a;
+     printf("enter a number");
+     scanf("%d",&a);
+     return 0;
+}
---
+++ 2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+
+	
+	int a ; 
+	scanf("%d",&a);
+	printf("autocomplete is very helpful in this laptop %d",a);
+	
+}
